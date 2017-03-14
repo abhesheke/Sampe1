9d4f7a9ff38ff6da86b4669b979048b8f7d6e72e
aabhesheke <aabhesheke@USLTP0075.csilhyd1.com>
2017-03-14 18:30:00 +0530

    Adding commit for pull request

--- SampleMinnu/src/test/java/com/sample/minnu/Sample.java
+++ SampleMinnu/src/test/java/com/sample/minnu/Sample.java
@@ -5,7 +5,10 @@ public class Sample {
 
 	public static void main(String[] args) {
 
-		System.out.println("Tesing");
+		System.out.println("Testing-----1");
+		System.out.println("Testing-----2");
+		System.out.println("Git Pull Request");
+
 	}
 
 }
