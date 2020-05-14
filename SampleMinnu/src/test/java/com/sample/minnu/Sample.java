50bfadaf7c28dcd5900e4b2d9e31aebf6704c5a9
aabhesheke <aabhesheke@L01LTP0087.csilhyd1.com>
2020-05-14 12:47:26 +0530

    Added Code for branch 1

--- SampleMinnu/src/test/java/com/sample/minnu/Sample.java
+++ SampleMinnu/src/test/java/com/sample/minnu/Sample.java
@@ -7,6 +7,7 @@ public class Sample {
 @Test
 public void SampleTest() {
 
+	System.out.println("Branch Code");
 		System.out.println("Testing-----1");
 		System.out.println("Testing-----2");
 		System.out.println("Git Pull Request");
